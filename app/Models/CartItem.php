ae03aef192d09b1eda2944b78fce666bb8110299
3B132016 <tsengjay0gmail.com>
2025-01-01 01:31:22 +0800

    呈現使用者目前購物車的內容

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -1,24 +1,70 @@
 <?php
 
-namespace App\Models;
+namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\CartItem;
+use App\Http\Requests\StoreCartItemRequest;
+use App\Http\Requests\UpdateCartItemRequest;
 
-class CartItem extends Model
+class CartItemController extends Controller
 {
-    use HasFactory;
-    public $fillable=[
-        'product_id',
-    ];
-
-    public function user()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        return $this->belongsTo(User::class);
+        $cartItems = auth()->user()->cartItems;
+        $data = [
+            'cartItems'=>$cartItems,
+        ];
+        return view('cart_items.index', $data);
     }
 
-    public function product()
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
     {
-        return $this->belongsTo(Product::class);
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCartItemRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CartItem $cartItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CartItem $cartItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCartItemRequest $request, CartItem $cartItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CartItem $cartItem)
+    {
+        //
     }
 }
